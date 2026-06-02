db063e11a1276600eb5acf5f59ec0f7595329558
dinapoliluigi08-pixel <dinapoli.luigi08@gmail.com>
2026-06-02 16:11:59 +0200

    Add viewport meta tag for responsive design

--- registra.php
+++ registra.php
@@ -57,6 +57,7 @@ if (isset($_POST['registra'])) {
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrazione – Dinateca</title>
     <link rel="stylesheet" href="stile.css">
 </head>
