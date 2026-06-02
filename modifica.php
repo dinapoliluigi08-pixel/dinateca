1a784b8d5c75d631c154e55940bf5db4932bc130
dinapoliluigi08-pixel <dinapoli.luigi08@gmail.com>
2026-06-02 16:13:50 +0200

    Update modifica.php

--- modifica.php
+++ modifica.php
@@ -21,6 +21,7 @@ $colori_preset  = [
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifica – Dinateca</title>
     <link rel="stylesheet" href="stile.css">
 </head>
